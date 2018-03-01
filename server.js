331f63c01ecb2adf1713ce9ed50eff329526ac57
gwickstrom <gwick001@ucr.edu>
2018-03-01 08:27:51 -0800

    Short assignment for landing page review

---
+++ server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (request, response){
+    if(request.url === '/'){
+        fs.readFile('index.html', 'utf8', function(errors,contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/ninjas.html', 'utf8',          function(errors, contents){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(contents);
+        response.end();
+    });
+    else if(request.url === '/dojos.html', 'utf8', function(errors, contents){
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(contents);
+        response.end();
+    });
+    else{
+        response.writeHead(404);
+        response.end("File not found!");
+    }
+});
+
+server.listen(8000);
